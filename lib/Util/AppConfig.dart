fb0b284808f00bed53fb56744afc52ad37aaaab6
Jonathan Siddle <jonathan@jonathansiddle.com>
2020-10-15 17:17:43 +0100

    Inc version num

--- lib/Util/AppConfig.dart
+++ lib/Util/AppConfig.dart
@@ -1,6 +1,6 @@
 
 class AppConfig {
-  static final String appVersion = '1.0.8+8';
+  static final String appVersion = '1.0.11+11';
   static final String overview = 
   '''
   Squiddy is a client for Octopus Energy.
